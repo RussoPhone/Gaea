5e5f04e5d675ca8f0cdcda1c369ff6a951c00349
RussoPhone <100falecido@gmail.com>
2026-07-29 23:05:26 -0300

    aumentado o tamanho do mundo na seed 42

--- core/simulation/config.py
+++ core/simulation/config.py
@@ -3,8 +3,10 @@ from dataclasses import dataclass
 @dataclass
 class ScenarioConfig: #Config central de um cenario de simulação
     seed: int = 42
-    world_width: int = 20
-    world_height: int = 10
+    num_water_tiles = 15
+    num_food_tiles = 10
+    world_width: int = 30
+    world_height: int = 20
     num_organism: int = 10
     mtksptk: int = 24
     simulation_duration: int = 120
