792019a7f1cf26166dfefad1d8b31fc1f8149e20
RussoPhone <100falecido@gmail.com>
2026-07-29 23:05:58 -0300

    posicionamento de tiles mais dinamico para melhor sobrevivencia

--- core/simulation/scenario.py
+++ core/simulation/scenario.py
@@ -2,24 +2,32 @@ import random
 
 from core.ambient.world import World
 from core.ambient.sky import Sky
-from core.ambient.tile import WATER, FOOD
+from core.ambient.tile import WATER, FOOD, GRASS
 from core.being.organism import Organism
 from core.simulation.renderer import Renderer
 from core.simulation.gtime import Gtime
 from core.simulation.simulation import Simulation
 
+
+def place_tiles(world, tile, amount):
+    placed = 0
+
+    while placed < amount:
+        x = random.randint(0, world.width -1)
+        y = random.randint(0, world.height -1)
+
+        if world.get_tile(x, y) == GRASS:
+            world.set_tile(x, y, tile)
+            placed += 1
+
 def build_scenario(config): #monta um cenario completo a partir da scenarioconfig
-    random.seed(config.seed)
-
+    random.seed(config.seed) 
+    
     world = World(config.world_width, config.world_height)
-
-    water_x = random.randint(0, config.world_width -1) 
-    water_y = random.randint(0, config.world_height -1)
-    world.set_tile(water_x, water_y, WATER)
-
-    food_x = random.randint(0, config.world_width -1)
-    food_y = random.randint(0, config.world_height -1)
-    world.set_tile(food_x, food_y, FOOD) 
+    
+    place_tiles(world, WATER, config.num_water_tiles)
+    place_tiles(world, FOOD, config.num_food_tiles)
+    
 
     for i in range(config.num_organism):
         x = random.randint(0, config.world_width -1)
