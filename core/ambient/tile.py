e2e5e2314d948984a53b795cc109e8da5e57a81c
RussoPhone <100falecido@gmail.com>
2026-07-30 11:11:14 -0300

    novos simbolos pra melhor visualização

--- core/ambient/tile.py
+++ core/ambient/tile.py
@@ -4,6 +4,6 @@ class Tile: #aqui fica os tiles. As peças no tabuleiro.
         self.symbol = symbol
 #as tiles. Tipo e simbolo.
 GRASS = Tile("grass", ".")
-STONE = Tile("stone", "^")
-WATER = Tile("water", "~")
-FOOD = Tile("food", "*")
+STONE = Tile("stone", "S")
+WATER = Tile("water", "W")
+FOOD = Tile("food", "F")
