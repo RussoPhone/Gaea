3e87790bce61a2f00a790fe2950e3515b5c0d343
RussoPhone <100falecido@gmail.com>
2026-07-31 19:44:27 -0300

    nao pisca mais durante passagem de dia

--- core/simulation/renderer.py
+++ core/simulation/renderer.py
@@ -8,11 +8,11 @@ class Renderer: #aqui é onde se configura os paraetros da renderização.
             for x, tile in enumerate(row):
                 entity = world.get_entity_at(x, y)
 
-                if light < 0.25: #aqui é a função de renderização da luz.
-                    symbols.append(" ")
-                elif light <0.50:
-                    symbols.append(".")
-                elif entity is not None: #também renderiza a entidade junto a luz
+                #if light < 0.25: #aqui é a função de renderização da luz.
+                #    symbols.append(" ")
+                #elif light <0.50:
+                #    symbols.append(".")
+                if entity is not None: #também renderiza a entidade junto a luz
                     symbols.append(entity.symbol)
                 else:
                     symbols.append(tile.symbol)
