684bc77e9287638aa0983ee3e703bee84948dbbd
RussoPhone <100falecido@gmail.com>
2026-07-31 19:44:51 -0300

    edit de parametro

--- toys/toy001_survival_instinct/run.py
+++ toys/toy001_survival_instinct/run.py
@@ -1,4 +1,4 @@
 from toys.toy001_survival_instinct.world import build_toy_world 
 
-simulation = build_toy_world(seed=42, num_organism=10, duration=300)
+simulation = build_toy_world(seed=43, num_organism=5, duration=300)
 simulation.run(render_enabled=True)
