3134a78cc6d6713c24db55fe352dcaddb427454b
RussoPhone <100falecido@gmail.com>
2026-08-01 23:08:19 -0300

    ajuste basico do body

--- core/being/body.py
+++ core/being/body.py
@@ -9,8 +9,8 @@ class Body:
         if not self.alive:
             return
 
-        self.hunger += 0.05
-        self.thirst += 0.1 
+        self.hunger += 0.5
+        self.thirst += 0.5 
 
         if self.hunger >= 100 or self.thirst >= 100:
             self.alive = False
