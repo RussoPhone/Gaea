21d81f393a478e4e0526bf8358d1833e41b4d1b4
RussoPhone <100falecido@gmail.com>
2026-08-03 17:37:11 -0300

    troca de simbolo de stone

--- core/ambient/tile.py
+++ core/ambient/tile.py
@@ -1,9 +1,10 @@
 class Tile: #aqui fica os tiles. As peças no tabuleiro.
-    def __init__(self, tile_type, symbol): #terá seu nome tipo e seu simbolo representante.
+    def __init__(self, tile_type, symbol, blocking=False): #terá seu nome tipo e seu simbolo representante.
         self.tile_type = tile_type
         self.symbol = symbol
+        self.blocking = blocking 
 #as tiles. Tipo e simbolo.
 GRASS = Tile("grass", ".")
-STONE = Tile("stone", "S")
+STONE = Tile("stone", "^", blocking=True)
 WATER = Tile("water", "~")
 FOOD = Tile("food", "*")
