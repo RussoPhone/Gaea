fd53bc81eb9bc9541b1c741ca4acb2d12f5133c6
RussoPhone <100falecido@gmail.com>
2026-08-04 19:49:58 -0300

    novamentre trocado self.mtk pra self.mtksptk

--- core/simulation/simulation.py
+++ core/simulation/simulation.py
@@ -212,9 +212,9 @@ class Simulation: #È aqui que fica os parametros da simulação.
 
     def run(self, render_enabled=True, steps_per_frame=1):
         #steps_per_frame=1 aceleração. Roda diversos ticks entre cada frame redenrizado.
-        while self.gtime.mtk < self.simulation_duration:
+        while self.gtime.mtksptk < self.simulation_duration:
             for _ in range(steps_per_frame):
-                if self.gtime.mtk >= self.simulation_duration:
+                if self.gtime.mtksptk >= self.simulation_duration:
                     break 
                 self.step()
 
