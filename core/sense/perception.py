03d74ef0ec29719036321e9f16d9823f72d21230
RussoPhone <100falecido@gmail.com>
2026-08-03 22:48:36 -0300

    investigando

--- core/sense/perception.py
+++ core/sense/perception.py
@@ -4,7 +4,7 @@ class Perception:
 
     def add(self, spatial, tile, occupied):
         self.reading.append({
-            "distance": spatial.distance(),
+            "distance": spatial.distance(self),
             "dx" : spatial.dx,
             "dy": spatial.dy,
             "surface": tile.tile_type,
