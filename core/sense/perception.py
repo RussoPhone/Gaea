ff05327c7bfbca6926332ebe94e28146da2e7119
RussoPhone <100falecido@gmail.com>
2026-08-04 16:36:21 -0300

    corrigido problema de endereçamento

--- core/sense/perception.py
+++ core/sense/perception.py
@@ -4,7 +4,7 @@ class Perception:
 
     def add(self, spatial, tile, occupied):
         self.reading.append({
-            "distance": spatial.distance(self),
+            "distance": spatial.distance(),
             "dx" : spatial.dx,
             "dy": spatial.dy,
             "surface": tile.tile_type,
